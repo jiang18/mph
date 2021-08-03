d87a3c443de7fc3abf2f14d656ef9b595423b70c
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-08-03 13:41:00 -0400

    Update parse_mph_log.pl

--- scripts/parse_mph_log.pl
+++ scripts/parse_mph_log.pl
@@ -5,37 +5,28 @@ use warnings;
 my ($log, $out) = @ARGV;
 
 my @info;
-my @keys = qw(trt chol lu iter diff last_is_chol);
-my ($trt, $chol, $lu, $iter, $diff, $last_is_chol, $last_chol);
-$chol = 1;
-$lu = 0;
+my @keys = qw(trt rcond grad_norm iter diff);
+my ($trt, $rcond, $grad_norm, $iter, $diff);
 open IN,$log or die "Could not find $log: $!\n";
 while(<IN>) {
   if(/trait_name with ARG (.*)/) {
     $trt = $1;
   }
-  if(/completed normally/) {
-    $chol ++;
-    $last_chol = $iter + 1;
+  if(/number of H \= ([\d\-\.e]+)/) {
+    $rcond = $1;
   }
-  if(/Switched to LU/) {
-    $lu ++;
+  if(/Gradient norm \= ([\d\-\.e]+)/) {
+    $grad_norm = $1;
   }
-  if(/diff for all VCs = ([\d\.]+)/) {
-    $diff = $1;
-  }
-  if(/Completed MINQUE iteration (\d+)/) {
+  if(/Started MINQUE iteration (\d+)/) {
     $iter ++;
   }
+  if(/delta logLL = ([\d\.]+)/) {
+    $diff = $1;
+  }
+  
   if(/Analysis finished/) {
-    if($last_chol == $iter) {
-      $last_is_chol = "TRUE"
-    } else {
-      $last_is_chol = "FALSE";
-    }
-    push @info, {trt=>$trt, chol=>$chol, lu=>$lu, diff=>$diff, iter=>$iter, last_is_chol=>$last_is_chol};
-    $chol = 1;
-    $lu = 0;
+    push @info, {trt=>$trt, rcond=>$rcond, grad_norm=>$grad_norm, iter=>$iter, diff=>$diff};
     $iter = 0;
   }
 }
