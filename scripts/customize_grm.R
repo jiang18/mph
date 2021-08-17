8dbe06d072197792da6d2b0e599827c984c8c722
Jicai Jiang <jiang18@terpmail.umd.edu>
2021-08-17 00:17:04 -0400

    Update customize_grm.R

--- scripts/customize_grm.R
+++ scripts/customize_grm.R
@@ -8,16 +8,23 @@ grm = grm + t(grm)
 # IDs
 id = c(1:5)
 # sample size
-np = as.integer(5)
+np = 5
 # number of markers set to 1 for pseudo GRMs
 nm = 1
 
 con = file(gbin, "wb")
-writeBin(con=gbin, object=np)
-writeBin(con=gbin, object=as.numeric( c(nm,grm[lower.tri(grm, diag=TRUE)]) ), size=4)
-closeAllConnections()
+writeBin(as.integer(np), con, size=4)
+writeBin(as.numeric( c(nm,grm[lower.tri(grm, diag=TRUE)]) ), con, size=4)
+close(con)
 
 write.table(id, file=gid, quote=F, row.names=F, col.names=F)
 
-# check files
+# check .bin file
+mph_file = file(gbin, "rb")
+np = readBin(mph_file, n=1, what=integer(), size=4)
+nm = readBin(mph_file, n=1, what=numeric(), size=4)
+mph_G = matrix(0, nrow=np, ncol=np)
+mph_G[lower.tri(mph_G, diag=TRUE)] = readBin(mph_file, n=np*(np+1)/2, what=numeric(), size=4)
+close(mph_file)
+
 
