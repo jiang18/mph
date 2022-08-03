136f316a7f12fbd4650a9ca1460d10f3dc28f08e
Jicai Jiang <jiang18@terpmail.umd.edu>
2022-08-03 19:46:30 -0400

    Update grm_txt2bin.R

--- scripts/grm_txt2bin.R
+++ scripts/grm_txt2bin.R
@@ -5,19 +5,22 @@ library(data.table)
 args = commandArgs(trailingOnly=TRUE)
 # test if there are two arguments: if not, return an error
 if (length(args) != 2) {
-  stop("Two arguments must be supplied (input and output).n", call.=FALSE)
+  stop("Two arguments must be supplied (input and output)\n")
 }
 
 txtfile = args[1]
 prefix = args[2]
-print("Input filename prefix:", prefix)
+print(paste("Input filename prefix:", prefix))
 
 idfile = paste0(prefix, ".indi")
 binfile = paste0(prefix, ".bin")
 
 grm = as.matrix(fread(txtfile, head=F, check.names=F), rownames=1)
+print("Completed reading the input file.")
+
 id = rownames(grm)
 write.table(id, file=idfile, quote=F, row.names=F, col.names=F)
+print("Completed writing the indi file.")
 
 # sample size
 np = length(id)
@@ -27,3 +30,6 @@ con = file(binfile, "wb")
 writeBin(as.integer(np), con, size=4)
 writeBin(as.numeric( c(nm,grm[lower.tri(grm, diag=TRUE)]) ), con, size=4)
 close(con)
+print("Completed writing the bin file.")
+
+
