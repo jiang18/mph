faa00de4a64ade0076e743f676cbf429fd10b641
Jicai Jiang <jiang18@terpmail.umd.edu>
2023-10-23 23:36:25 -0400

    Update recalculate_enrichments.R

--- util/recalculate_enrichments.R
+++ util/recalculate_enrichments.R
@@ -1,44 +1,23 @@
-# Arguments
+# Recalculate the estimates and SEs of PVEs and enrichments from MPH VC estimates
 # vcfile: the .mq.vc.csv file produced by `mph --minque`.
-# snpinfo: SNP info file. Igonored if crossprod is provided.
-# crossprod: the crossproduct matrix of the weighting matrix in the SNP info file.
-# nsnps: the total number of SNPs. If not provided, it is set to the first GRM's number of SNPs in vcfile.
-# annot.size: a list of the number of SNPs in each output annotation category. If not provided, it is set to the "m" column of vcfile.
-# index: a list of column indices in snpinfo or crossprod matching vcfile. The first category should be indexed as 1, the second as 2, and so on.
-recalculate_enrichments <- function(vcfile, snpinfo=NA, crossprod=NA, nsnps=NA, annot.size=NA, index=NULL) {
+# crossprod: the crossproduct of SNP incidence matrix and SNP weighting matrix. Its row names and columns should match the annotation categories of interest and the rows of vcfile, respectively.
+# nsnps: the total number of SNPs. If `NA`, it is set to the first GRM's number of SNPs in vcfile.
+# annot.size: a list of the number of SNPs in each annotation category listed in the row names of crossprod. It can be computed from the column-wise sum of the corresponding SNP incidence matrix. If `NA`, it is set to the `m` column of vcfile.
+recalculate_enrichments <- function(vcfile, crossprod, nsnps=NA, annot.size=NA) {
     if(is.na(vcfile)) {
         stop("vcfile must be specifiled.")
     }
-
-    if(is.na(snpinfo) && length(crossprod) == 1 && is.na(crossprod)) {
-        stop("Either snpinfo or crossprod must be specified.")
-    }
-
-    if(! is.matrix(crossprod)) {
-        library(data.table)
-        sw = fread(snpinfo, head=T, sep=",")
-        annot = colnames(sw)[-1]
-        snp = sw$SNP
-        sw = sw[,-1]
-        sw[is.na(sw)] = 0
-        sw = as.matrix(sw)
-        crossprod = crossprod(sw)
+    if(length(crossprod) == 1 && is.na(crossprod)) {
+        stop("crossprod must be specified.")
     }
 
     mq = read.csv(vcfile)
     mq = mq[-nrow(mq),]
-    if(length(index) == 0) {
-        index = c(1:ncol(crossprod))
+    if(ncol(crossprod) > nrow(mq)) {
+        stop(paste("ncol(crossprod) should equal the number of genetic VCs in", vcfile))
     }
-    if(max(index) > ncol(crossprod)) {
-        stop("Dim of snpinfo or crossprod NOT consistent with index specified.")
-    }
-    if(length(index) > nrow(mq)) {
-        stop(paste("Length of index must be <= the number of genetic VCs in", vcfile))
-    }
-    mq = mq[1:length(index),]
+    mq = mq[1:ncol(crossprod),]
 
-    lhs = crossprod[, index]
     # PVE estimate
     var = lhs %*% as.matrix(mq[,9:(8+nrow(mq))]) %*% t(lhs)
     pve.est = lhs %*% mq$enrichment / sum(mq$m)
