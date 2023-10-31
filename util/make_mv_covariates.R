20117d1246b9ba6da1c2186e5018f994f737721f
Jicai Jiang <jiang18@terpmail.umd.edu>
2023-10-31 11:09:51 -0400

    Update multivariate scripts

---
+++ util/make_mv_covariates.R
@@ -0,0 +1,30 @@
+# Generates a multivariate covariate file
+# Oct 31, 2023: initial release along with MPH v0.49.2
+
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) == 2) {
+    infile = args[1]
+    outfile = args[2]
+    ntrt = 2
+} else if(length(args) == 3) {
+    infile = args[1]
+    outfile = args[2]
+    ntrt = as.integer(args[3])
+} else {
+    stop("Two or three arguments must be supplied:\n  input covariate CSV filename, output CSV filename, and number of traits (optional)\n", call.=FALSE)
+}
+
+covar = read.csv(infile)
+dim = nrow(covar)
+nc = ncol(covar)-1
+iid = paste(rep(1:ntrt, each=dim), rep(covar[,1], ntrt), sep=".")
+
+out = matrix(0, nrow=dim*ntrt, ncol=ntrt*nc+1)
+out[,1] = iid
+for(i in 1:ntrt) {
+    out[(1+(i-1)*dim):(i*dim), (2+(i-1)*nc):(1+i*nc)] = as.matrix(covar[,-1])
+}
+colnames(out) = c("IID", paste(rep(1:ntrt, each=nc), rep(colnames(covar)[-1], ntrt), sep="."))
+write.csv(out, file=outfile, quote=F, row.names=F, na="")
---
+++ util/make_mv_erms.R
@@ -0,0 +1,42 @@
+# Generate multivariate environmental relationship matrices
+# Oct 31, 2023: initial release along with MPH v0.49.2
+
+#!/usr/bin/env Rscript
+
+source("mph_functs.R")
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) == 2) {
+    infile = args[1]
+    outfile = args[2]
+    ntrt = 2
+} else if(length(args) == 3) {
+    infile = args[1]
+    outfile = args[2]
+    ntrt = as.integer(args[3])
+} else {
+    stop("Two or three arguments must be supplied:\n  input GRM filename prefix, output filename prefix, and number of traits (optional).\n", call.=FALSE)
+}
+
+if(ntrt > 9) {
+    stop("Too many traits: The max number of traits is limited to 9.\n")
+}
+
+keep = read.table(paste0(infile, ".grm.iid"))
+dim = length(keep[,1])
+iid = paste(rep(1:ntrt, each=dim), rep(keep[,1], ntrt), sep=".")
+
+for(i in 1:(ntrt-1)) {
+    full = matrix(0, nrow=dim*ntrt, ncol=dim*ntrt)
+    diag(full[(1+(i-1)*dim):(dim*i), (1+(i-1)*dim):(dim*i)]) = 1
+    write_grm(paste0(outfile, ".E", i, "d"), iid, full)
+}
+
+for(i in 1:(ntrt-1)) {
+    for(j in (i+1):ntrt) {
+        full = matrix(0, nrow=dim*ntrt, ncol=dim*ntrt)
+        diag(full[(1+(i-1)*dim):(dim*i), (1+(j-1)*dim):(dim*j)]) = 1
+        diag(full[(1+(j-1)*dim):(dim*j), (1+(i-1)*dim):(dim*i)]) = 1
+        write_grm(paste0(outfile, ".E", i, j), iid, full)
+    }
+}
