43de7aa9e2438c98fd0fb830f7a925b2f65e9e0a
Jicai Jiang <jiang18@terpmail.umd.edu>
2023-10-31 17:46:43 -0400

    Update make_gci_grm.R

--- util/make_gci_grm.R
+++ util/make_gci_grm.R
@@ -1,4 +1,4 @@
-# Generate a genotype–covariate interaction GRM from a routine GRM and an incidence matrix
+# Generate a genotype–covariate interaction GRM from a routine GRM and an indicator matrix.
 # Oct 29, 2023: initial release along with MPH v0.49.2
 
 #!/usr/bin/env Rscript
